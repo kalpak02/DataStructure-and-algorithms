7ec05e21268318f15e021dbe8cf76d6933f207cf
kalpak  buradkar <kalpak47k@gmail.com>
2026-05-17 16:15:17 +0530

    Add files via upload

---
+++ trees/Tree_Implementation.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+Node *buildTree(int data)
+{
+    // -1 data indicates that we have the leaf node (Base Case)
+    if (data == -1)
+    {
+        return NULL;
+    }
+    // Create the root node and hence solved 1 case
+    Node *root = new Node(data);
+    // Recursion will handle
+    int leftData;
+    cout << "Enter the data for left child of " << data << endl;
+    cin >> leftData;
+    root->left = buildTree(leftData);
+    int rightData;
+    cout << "Enter the data for right child of " << data << endl;
+    cin >> rightData;
+    root->right = buildTree(rightData);
+    return root;
+}
+
+
+
+// class Node
+// {
+// public:
+//     int data;
+//     Node *left;
+//     Node *right;
+
+//     Node(int data)
+//     {
+//         this->data = data;
+//         left = NULL;
+//         right = NULL;
+//     }
+
+// };
+
+// Node *buildTree(int data)
+//     {
+//         if (data == -1)
+//         {
+//             return NULL;
+//         }
+
+//         Node *root = new Node(data);
+
+//         int leftdata;
+//         cout << "enter the data for left of " << data << endl;
+//         cin >> leftdata;
+//         root->left = buildTree(leftdata);
+
+//         int rightdata;
+//         cout << "enter the data for right of " << data << endl;
+//         cin >> rightdata;
+//         root->right = buildTree(rightdata);
+
+//         return root;
+//     }
+
+
+
+
+// int main()
+// {
+//     Node *root;
+//     int data;
+//     cout << "Enter data for root node" << endl;
+//     cin >> data;
+//     root = buildTree(data);
+//     return 0;
+// }
